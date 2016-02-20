57bedc15fb7a0ee3804d02986116427a472fcf7b
dong hoon kim <donghoonkim1226@gmail.com>
2016-02-20 14:07:27 -0500

    pushing up broken code as a template

--- server.js
+++ server.js
@@ -10,7 +10,25 @@ var PORT = process.env.NODE_ENV || 3000;
 
 var app = express();
 
-var User_information = sequelize.define
+var User_information = sequelize.define('user_information', {
+	user_name: {
+		type: Sequelize.STRING,
+		unique: true,
+		validate: {
+			notNull: true,
+		}
+	},
+	password: {
+		type: Sequelize.STRING,
+		validate: {
+			notNull: true,
+			// len: {
+			// 	args: [4, 24],
+			// 	msg: "Choose a different password"
+			// }
+		}
+	}
+});
 
 app.engine('handlebars', expressHandelbars({
 	defaultLayout: 'main'
@@ -21,6 +39,40 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.get('/fail', function(req, res) {
+	res.send("failed!");
+});
+
+app.post('/login', function(req, res) {
+	user_information.findOne({
+		where: {
+			user_name: req.body.user_name
+		}
+	})
+});
+
+app.post('/registration', function(req, res) {
+	bcrypt.hash(req.body.password, 10, function(err, hash) {
+		if(err) {throw err};
+
+		var name = req.body.user_name;
+		var password = hash;
+		console.log(name, hash);
+		User_information.create({
+			user_name: name,
+			password: hash
+		}).then(function(result) {
+			res.redirect('/')
+		}).catch(function(err) {
+			res.redirect('/fail')
+		})
+	})
+});
 
 sequelize.sync().then(function() {
 	app.listen(PORT, function() {
